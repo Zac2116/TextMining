cc1aaf1fe5b7e60039d25ebeab420d58ee6f8857
ZacWu <zw2338@columbia.edu>
2016-04-14 20:04:08 -0400

    update

---
+++ lib/cleanTypo.py
@@ -0,0 +1,21 @@
+def cleanTypo(word):
+    
+    isTypo = False
+    index = 0
+    char_list = list(word)
+    three_chars = ngrams(char_list,3)
+    for i in range(len(three_chars)):
+        c1 = 'a' <= three_chars[i][0] <= 'z'
+        c2 = 'A' <= three_chars[i][1] <= 'Z'
+        c3 = 'a' <= three_chars[i][0] <= 'z'
+        isTypo = isTypo or c1&c2&c3
+        if isTypo:
+            index = i+1
+            cor_word = word[:index] + " " + word[index:]
+            break
+
+        else:
+            cor_word = word
+
+        
+    return cor_word.split(" ")
--- lib/dfeature.py
+++ lib/dfeature.py
@@ -8,3 +8,11 @@ def jCDistance(A,B):
 
 	jC = len(deno) / float(len(nom))
 	return jC
+
+
+def DicDist(A,B):
+	#size(2 times intersection of A and B )/ size(((A) + (B)))
+	deno = set(A) & set(B)
+	nom = len(set(A)) + len(set(B))
+	DD = len(deno)/float(nom)
+	return DD
