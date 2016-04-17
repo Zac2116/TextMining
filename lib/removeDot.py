a15e4ca5caa5990c0c3623a865de546f0b112b44
xiaocixing <xx2203@columbia.edu>
2016-04-17 13:37:15 -0400

    Update removeDot.py

--- lib/removeDot.py
+++ lib/removeDot.py
@@ -1,3 +1,4 @@
+'''
 import re
 def removeDot(s):
 	if s[0] == '.':
@@ -5,3 +6,16 @@ def removeDot(s):
 	if s[len(s)-1] == '.':
 		s = s[:-1]
 	return s
+'''
+def removeDot(s):
+        num = ["0","1","2","3","4","5","6","7","8","9"]
+        if s[0] == ".":
+                s = removeDot(s[1:])
+        elif s[len(s)-1]==".":
+                s = removeDot(s[:-1])
+        else:
+                for i in range(1,len(s)-1):
+                        if s[i]=="." and (s[i-1] not in num or s[i+1] not in num):
+                                s = removeDot(s[:i]+s[i+1:])
+                                break
+        return s
